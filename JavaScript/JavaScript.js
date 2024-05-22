9bc9d95c87e6f86dfc545f85ca01281781192641
GiovannyEnes <163896641+GiovannyEnes@users.noreply.github.com>
2024-05-22 09:18:52 -0300

    Final

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,9 @@
+function enviar() {
+    swal("Formulário recebido!", "Obrigado por entrar em contato conosco! \n Iremos analisar seu curriculo e retornaremos!", "successo");
+}
+
+function aumentartexto() {
+    document.getElementById("texto-limitado").style.fontSize = "20px";
+
+}
+
